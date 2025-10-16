f698c64cd1402f643964492b1655a3878748fe0b
Fernandes-Bernardo <bernardofpdev@gmail.com>
2025-10-16 18:29:57 -0300

    fix: adjusting setpoint value

--- src/main/java/frc/robot/subsystems/IntakeBola.java
+++ src/main/java/frc/robot/subsystems/IntakeBola.java
@@ -7,19 +7,21 @@ import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 
 public class IntakeBola extends ExampleSubsystem {
 
     // Controle
     XboxController driveController = new XboxController(0);
 
+    // Sensor para coleta
+    public DigitalInput sensor = new DigitalInput(1);
+
     // SparkMax e Enconder
     public static SparkMax motorIntakeBola = new SparkMax(2, MotorType.kBrushless);
-    public static SparkMax motorColeta = new SparkMax(18, MotorType.kBrushless);
+    public static SparkMax motorColeta = new SparkMax(16, MotorType.kBrushless);
     public RelativeEncoder encoder_bola;
     private SparkMaxConfig motor_intakeBConf;
     private SparkMaxConfig motorColetaConf;
@@ -36,6 +38,7 @@ public class IntakeBola extends ExampleSubsystem {
         // Configurando SparkMax coleta
         motorColetaConf = new SparkMaxConfig();
         motorColetaConf.idleMode(IdleMode.kBrake);
+        motorColetaConf.inverted(true);
 
         motorColeta.configure(motorColetaConf, null, PersistMode.kNoPersistParameters);
 
@@ -46,12 +49,12 @@ public class IntakeBola extends ExampleSubsystem {
     public void setReferencia(double setpoint) {
 
         if (setpoint > 0.5) {
-            motor_intakeBConf.closedLoop.p(0.05);
+            motor_intakeBConf.closedLoop.p(0.05); // talvez 0.09
             motorIntakeBola.configure(motor_intakeBConf, null, PersistMode.kNoPersistParameters);
             motorIntakeBola.getClosedLoopController().setReference(setpoint, ControlType.kPosition);
 
         } else {
-            motor_intakeBConf.closedLoop.p(1);
+            motor_intakeBConf.closedLoop.p(0.8);
             motorIntakeBola.configure(motor_intakeBConf, null, PersistMode.kNoPersistParameters);
             motorIntakeBola.getClosedLoopController().setReference(setpoint, ControlType.kPosition);
         }
@@ -62,21 +65,27 @@ public class IntakeBola extends ExampleSubsystem {
 
         // Articulação
         if (driveController.getAButton()) {
-            setReferencia(7.3);
-            // Adicionar lógica do sensor para parar a coleta
+            setReferencia(7.6);
         } else if (driveController.getXButton()) {
             setReferencia(1);
         } 
 
         // Rodinhas coleta
         if (driveController.getRightBumperButton()){
-            motorColeta.set(1);
+            motorColeta.set(0.5);
         } else if(driveController.getLeftBumperButton()){
-            motorColeta.set(-1);
+            motorColeta.set(-0.7);
+        } else {
+            motorColeta.disable();
         }
     }
 
     public void dashboard() {
         SmartDashboard.putNumber("Posicao Atual motor intakeBola: ", encoder_bola.getPosition());
+    }
+
+    // sensor para coleta
+    public boolean ativado(){
+        return sensor.get();
     }
 }
