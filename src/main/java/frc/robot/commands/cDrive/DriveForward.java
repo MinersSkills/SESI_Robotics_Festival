49a050307a17586ecdeaba4602902a3dbcedd4bd
Fernandes-Bernardo <bernardofpdev@gmail.com>
2025-10-18 13:47:38 -0300

    feat: adding drive commands

---
+++ src/main/java/frc/robot/commands/cDrive/DriveForward.java
@@ -0,0 +1,51 @@
+package frc.robot.commands.cDrive;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.drive.Drive;
+
+public class DriveForward extends Command {
+
+    private final Drive drive;
+    private final double speed;
+    private final double duration;
+    private final Timer timer = new Timer();
+
+    public DriveForward(Drive drive, double speed, double duration) {
+        this.drive = drive;
+        this.speed = speed;
+        this.duration = duration;
+        addRequirements(drive);
+    }
+
+    @Override
+    public void initialize() {
+        timer.reset();
+        timer.start();
+    }
+
+    @Override
+    public void execute() {
+        drive.frenteDireita.set(-speed);
+        drive.trasDireita.set(-speed);
+        drive.frenteEsquerda.set(-speed);
+        drive.trasEsquerda.set(-speed);
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        drive.frenteDireita.disable();
+        drive.trasDireita.disable();
+        drive.frenteEsquerda.disable();
+        drive.trasEsquerda.disable(); 
+    }
+
+    @Override
+    public boolean isFinished() {
+        if (timer.get() >= duration) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/frc/robot/commands/cDrive/DriveLeft.java
@@ -0,0 +1,47 @@
+package frc.robot.commands.cDrive;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.drive.Drive;
+
+public class DriveLeft extends Command {
+
+    private final Drive drive;
+    private final double speed;
+    private final double duration;
+    private final Timer timer = new Timer();
+
+    public DriveLeft(Drive drive, double speed, double duration) {
+        this.drive = drive;
+        this.speed = speed;
+        this.duration = duration;
+        addRequirements(drive);
+    }
+
+    @Override
+    public void initialize() {
+        timer.reset();
+        timer.start();
+    }
+
+    @Override
+    public void execute() {
+        drive.frenteEsquerda.set(-speed);
+        drive.trasEsquerda.set(-speed);
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        drive.frenteDireita.disable();
+        drive.trasDireita.disable();
+    }
+
+    @Override
+    public boolean isFinished() {
+        if (timer.get() >= duration) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
