ab7cef83e1b96b90e5603016c30f6b962628aedb
Fernandes-Bernardo <bernardofpdev@gmail.com>
2025-10-26 10:49:40 -0300

    feat: add drive constants

---
+++ src/main/java/frc/robot/constants/DriveConstants.java
@@ -0,0 +1,16 @@
+package frc.robot.constants;
+
+public class DriveConstants {
+    public class id{
+
+        public static final int ID_VICTOR_FRENTE_ESQUERDA = 3;
+        public static final int ID_VICTOR_FRENTE_DIREITA = 5;
+        public static final int ID_VICTOR_TRAS_DIREITA = 4;
+        public static final int ID_VICTOR_TRAS_ESQUERDA = 2;
+    }
+
+    public class axis{
+        public static final int DRIVE_AXIS = 1;
+        public static final int TURN_AXIS = 4;
+    }
+}
