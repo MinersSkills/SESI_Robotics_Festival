27a9a5025e00e3adec13d4e33af74ad52839cba9
Fernandes-Bernardo <bernardofpdev@gmail.com>
2025-10-26 10:51:08 -0300

    refactor: trading raw values to constants on drive class

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.constants.DriveConstants;
 import frc.robot.subsystems.ExampleSubsystem;
 
 
@@ -23,11 +24,11 @@ public class Drive extends ExampleSubsystem{
 
     // Inicializando ID dos VictorSPX
     public Drive(){
-        frenteEsquerda = new WPI_VictorSPX(3);
-        frenteDireita = new WPI_VictorSPX(5);
+        frenteEsquerda = new WPI_VictorSPX(DriveConstants.id.ID_VICTOR_FRENTE_ESQUERDA);
+        frenteDireita = new WPI_VictorSPX(DriveConstants.id.ID_VICTOR_FRENTE_DIREITA);
 
-        trasDireita = new WPI_VictorSPX(4);
-        trasEsquerda = new WPI_VictorSPX(2);
+        trasDireita = new WPI_VictorSPX(DriveConstants.id.ID_VICTOR_TRAS_DIREITA);
+        trasEsquerda = new WPI_VictorSPX(DriveConstants.id.ID_VICTOR_TRAS_ESQUERDA);
 
         //Invertendo motores
         frenteDireita.setInverted(true);
@@ -36,8 +37,8 @@ public class Drive extends ExampleSubsystem{
 
 
     public void drive(){
-        double velocidade = driveController.getRawAxis(1); // Movimenta frente e tras
-        double rotacao = driveController.getRawAxis(4); // Movimenta para os lados
+        double velocidade = driveController.getRawAxis(DriveConstants.axis.DRIVE_AXIS); // Movimenta frente e tras
+        double rotacao = driveController.getRawAxis(DriveConstants.axis.TURN_AXIS); // Movimenta para os lados
 
         double esquerda = velocidade + rotacao;
         double direita = velocidade - rotacao;
